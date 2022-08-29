7d36effa67a1b0695e83838c4b68a3a3f5357fea
Pascal Laliberté <pascal@hey.com>
2022-08-29 14:53:37 +0000

    chart_scaffolder: add usage instructions

--- lib/bullet_train/supercharts/scaffolders/chart_scaffolder.rb
+++ lib/bullet_train/supercharts/scaffolders/chart_scaffolder.rb
@@ -3,7 +3,21 @@ module BulletTrain
     module Scaffolders
       class ChartScaffolder < SuperScaffolding::Scaffolder
         def run
-          puts "This will scaffolder a chart. Super!"
+          unless argv.count >= 2
+            puts ""
+            puts "🚅  usage: bin/super-scaffold supercharts:chart <TargetModel> <ParentModel[s]>"
+            puts ""
+            puts "E.g. a chart on the team dashboard showing click-throughs per day"
+            puts "This is assuming that you've already got a ClickThrough model, capturing each campaign click-through as a separate record. The chart will default to group by day derived by the :created_at"
+            puts ""
+            puts "  bin/super-scaffold supercharts:chart ClickThrough Team"
+            puts ""
+            standard_protip
+            puts ""
+            exit
+          end
+
+          puts "This will scaffold a chart. Super!"
         end
       end
     end
