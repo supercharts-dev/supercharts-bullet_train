5dcc398ce029085d4f57cc73c2cd425f36d8ef0c
Pascal Laliberte <pascal@hey.com>
2025-09-17 17:25:27 -0400

    Deprecate gem (#29)
    * add post_install_message to gemspec
    
    * add deprecation warning on gem load
    
    * comment out scaffolders

--- lib/bullet_train/supercharts.rb
+++ lib/bullet_train/supercharts.rb
@@ -1,13 +1,21 @@
-require "bullet_train/supercharts/version"
-require "bullet_train/supercharts/engine"
-require "scaffolding"
-require "scaffolding/transformer"
-require "scaffolding/block_manipulator"
-require "scaffolding/routes_file_manipulator"
-require "bullet_train/supercharts/scaffolders/chart_scaffolder"
+# Issue deprecation warning when this gem is loaded
+unless defined?(BulletTrain::Supercharts::DEPRECATION_WARNING_SHOWN)
+  warn "⚠️  DEPRECATION WARNING: The supercharts-bullet_train gem is deprecated. Please eject the files locally using `bin/rails supercharts:eject_all`. See README for details."
+end
+
+# This gem is deprecated. All scaffolders are disabled.
+# require "bullet_train/supercharts/version"
+# require "bullet_train/supercharts/engine"
+# require "scaffolding"
+# require "scaffolding/transformer"
+# require "scaffolding/block_manipulator"
+# require "scaffolding/routes_file_manipulator"
+# require "bullet_train/supercharts/scaffolders/chart_scaffolder"
 
 module BulletTrain
   module Supercharts
+    # Mark that the deprecation warning has been shown
+    DEPRECATION_WARNING_SHOWN = true
     # Your code goes here...
   end
 end
