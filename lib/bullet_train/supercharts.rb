17921c16dfb77b0011ceeb19dcdaec96b02b9622
Pascal Laliberté <pascal@hey.com>
2025-09-17 17:24:33 -0400

    comment out scaffolders

--- lib/bullet_train/supercharts.rb
+++ lib/bullet_train/supercharts.rb
@@ -3,13 +3,14 @@ unless defined?(BulletTrain::Supercharts::DEPRECATION_WARNING_SHOWN)
   warn "⚠️  DEPRECATION WARNING: The supercharts-bullet_train gem is deprecated. Please eject the files locally using `bin/rails supercharts:eject_all`. See README for details."
 end
 
-require "bullet_train/supercharts/version"
-require "bullet_train/supercharts/engine"
-require "scaffolding"
-require "scaffolding/transformer"
-require "scaffolding/block_manipulator"
-require "scaffolding/routes_file_manipulator"
-require "bullet_train/supercharts/scaffolders/chart_scaffolder"
+# This gem is deprecated. All scaffolders are disabled.
+# require "bullet_train/supercharts/version"
+# require "bullet_train/supercharts/engine"
+# require "scaffolding"
+# require "scaffolding/transformer"
+# require "scaffolding/block_manipulator"
+# require "scaffolding/routes_file_manipulator"
+# require "bullet_train/supercharts/scaffolders/chart_scaffolder"
 
 module BulletTrain
   module Supercharts
